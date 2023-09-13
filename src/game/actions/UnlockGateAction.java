37da89ed9e06674ff18f567eaad963c67ce03d87
DesmondChongQiXiang <91426701+qixiangggg@users.noreply.github.com>
2023-09-13 17:54:13 +0800

    upload assignment1 code

---
+++ src/game/actions/UnlockGateAction.java
@@ -0,0 +1,56 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.capability.Ability;
+import game.ground.Gate;
+/**
+ * An Action that unlock the gate
+ */
+public class UnlockGateAction extends Action {
+    /**
+     * The target gate
+     */
+    private final Gate gate;
+
+    /**
+     * Constructor to create an Action that will unlock a gate
+     *
+     * @param gate the target gate we are unlocking
+     */
+
+    public UnlockGateAction(Gate gate){
+        this.gate = gate;
+    }
+
+    /**
+     * Allow the Actor to unlock a gate
+     *
+     * Overrides Action.execute()
+     *
+     * @see Action#execute(Actor, GameMap)
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a description of the Action suitable for the menu
+     */
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        if (actor.hasCapability(Ability.UNLOCK_GATE)){
+            gate.unlockGate();
+            return "Gate is now unlocked.";
+            }
+        return "Gate is locked shut.";
+        }
+
+    /**
+     * Returns a description of this movement suitable to display in the menu.
+     *
+     * @param actor The actor performing the action.
+     * @return a String, e.g. "Player unlock Gate"
+     */
+    @Override
+    public String menuDescription(Actor actor) {
+        return String.format("%s unlocks Gate",actor);
+    }
+}
---
+++ src/game/ground/Void.java
@@ -0,0 +1,26 @@
+package game.ground;
+
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+
+
+public class Void extends Ground{
+    /**
+     * Constructor.
+     */
+    public Void() {
+        super('+');
+    }
+
+    /**
+     * Void can kill an actor when the actor step on it
+     * @param location The location of the Ground
+     */
+    @Override
+    public void tick(Location location) {
+        if (location.containsAnActor()){
+            new Display().println(location.getActor().unconscious(location.map()));
+        }
+    }
+}
