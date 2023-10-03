66161d30d9877a71dede78d5cf851401562cd493
Chonghow29 <cong0027@student.monash.edu>
2023-10-04 00:51:53 +0800

    Documentation for grounds package

--- src/game/grounds/Floor.java
+++ src/game/grounds/Floor.java
@@ -5,25 +5,25 @@ import edu.monash.fit2099.engine.positions.Ground;
 import game.capabilities.Ability;
 
 /**
- * A class that represents the floor inside a building.
+ * The `Floor` class represents the floor inside a building. It is a type of ground environment
+ * and is represented by the character '_' on the game map.
  * Created by:
- * @author Riordan D. Alfredo
  * Modified by:
- * Desmond Chong Qi Xiang
  */
 public class Floor extends Ground {
+
     /**
-     * Constructor.
+     * Constructor to create a `Floor` instance.
      */
     public Floor() {
         super('_');
     }
 
     /**
-     * Returns a boolean whether the actor that tried to enter has the ability(enter floor)
+     * Checks whether the actor can enter this floor. Actors can enter if they have the ability to "enter floor."
      *
-     * @param actor the Actor that tried to enter
-     * @return a boolean
+     * @param actor the Actor attempting to enter
+     * @return true if the actor has the ability to enter, false otherwise
      */
     @Override
     public boolean canActorEnter(Actor actor) {
--- src/game/grounds/environments/EnemySpawnableGround.java
+++ src/game/grounds/environments/EnemySpawnableGround.java
@@ -4,15 +4,22 @@ import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
 import game.spawners.Spawner;
 
+/**
+ * The `EnemySpawnableGround` class represents a type of ground environment where enemies can spawn over time.
+ * It extends the `Ground` class and provides a mechanism for spawning enemies using a specified `Spawner`.
+ */
 public abstract class EnemySpawnableGround extends Ground {
 
   /**
-   * Enemy Spawner
+   * The spawner responsible for spawning enemies on this ground.
    */
   private Spawner spawner;
 
   /**
-   * Constructor.
+   * Constructs a new instance of the `EnemySpawnableGround` class.
+   *
+   * @param displayChar The character used to display this ground on the map.
+   * @param spawner     The spawner responsible for spawning enemies on this ground.
    */
   public EnemySpawnableGround(char displayChar, Spawner spawner){
     super(displayChar);
@@ -20,8 +27,9 @@ public abstract class EnemySpawnableGround extends Ground {
   }
 
   /**
-   * Graveyard can spawn an enemy over time.
-   * @param location The location of the Ground
+   * Called on each game tick to potentially spawn an enemy on this ground.
+   *
+   * @param location The location of this ground on the map.
    */
   @Override
   public void tick(Location location) {
