9f02602e87be5e141031cd73d3eaec8d5cb28f89
Chonghow29 <cong0027@student.monash.edu>
2023-10-05 13:38:49 +0800

    REQ5 (Java docstring for spawners package)

--- src/game/spawners/Spawner.java
+++ src/game/spawners/Spawner.java
@@ -6,6 +6,8 @@ import game.actors.enemies.Enemy;
 /**
  * An interface representing a spawner for enemy actors in the game world.
  * Classes implementing this interface are responsible for spawning enemy actors at specified locations.
+ *
+ * @author MA_AppliedSession1_Group7
  */
 public interface Spawner<E extends Enemy> {
 
