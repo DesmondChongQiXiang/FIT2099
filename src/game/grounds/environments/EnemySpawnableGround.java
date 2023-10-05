4f7b7a529019565fbab33da432cac336024ce531
Chonghow29 <cong0027@student.monash.edu>
2023-10-05 13:28:28 +0800

    REQ5 (Java docstring for grounds package)

--- src/game/grounds/environments/EnemySpawnableGround.java
+++ src/game/grounds/environments/EnemySpawnableGround.java
@@ -5,30 +5,46 @@ import edu.monash.fit2099.engine.positions.Location;
 import game.actors.enemies.Enemy;
 import game.spawners.Spawner;
 
+/**
+ * The EnemySpawnableGround class represents a type of ground in the game world where enemies can spawn over time.
+ * It extends the Ground class and provides a mechanism for controlling the spawning of enemies on this type of ground.
+ *
+ * This class defines the spawn rate and uses a spawner to create and add enemies to the location if the spawn condition is met.
+ *
+ * @author : MA_AppliedSession1_Group7
+ *
+ * @param <E> The type of enemy that can spawn on this type of ground.
+ * @see Ground
+ */
 public abstract class EnemySpawnableGround<E extends Enemy> extends Ground {
 
   /**
-   * the chance of spawn an enemy
+   * The chance of spawning an enemy on this type of ground.
    */
   protected int spawnRate;
 
   /**
-   * the spawner of enemy
+   * The spawner used to create and spawn enemies.
    */
   protected Spawner<E> spawner;
 
   /**
-   * Constructor.
+   * Constructor to create an EnemySpawnableGround instance.
+   *
+   * @param displayChar The character representation of this type of ground.
+   * @param spawnRate   The spawn rate at which enemies appear on this type of ground.
+   * @param spawner     The spawner for enemies associated with this type of ground.
    */
-  public EnemySpawnableGround(char displayChar, int spawnRate, Spawner spawner){
+  public EnemySpawnableGround(char displayChar, int spawnRate, Spawner<E> spawner){
     super(displayChar);
     this.spawnRate = spawnRate;
     this.spawner = spawner;
   }
 
   /**
-   * Graveyard can spawn an enemy over time.
-   * @param location The location of the Ground
+   * Called at each game tick to determine if an enemy should spawn on this type of ground.
+   *
+   * @param location The location of the ground.
    */
   @Override
   public void tick(Location location) {
@@ -37,7 +53,13 @@ public abstract class EnemySpawnableGround<E extends Enemy> extends Ground {
     }
   }
 
+  /**
+   * Sets the spawn rate for this type of ground.
+   *
+   * @param spawnRate The new spawn rate to set.
+   */
   public void setSpawnRate(int spawnRate) {
     this.spawnRate = spawnRate;
   }
 }
+
--- src/game/grounds/environments/forestenemyspawnableground/Bushes.java
+++ src/game/grounds/environments/forestenemyspawnableground/Bushes.java
@@ -6,15 +6,38 @@ import game.spawners.Spawner;
 import game.weathers.Weather;
 import game.weathers.WeatherControllable;
 
+/**
+ * The Bushes class represents a type of ground in the forest environment where Red Wolves can spawn.
+ * It extends the ForestEnemySpawnableGround class and provides functionality to update Red Wolf spawning rates
+ * based on the current weather conditions.
+ *
+ * The class adjusts Red Wolf spawning rates according to the weather. During sunny weather, Red Wolf spawning rates decrease,
+ * making them less active, while during rainy weather, the rates increase, making them more active.
+ *
+ * @author : MA_AppliedSession1_Group7
+ *
+ * @param <R> The type of Red Wolf spawner associated with this Bushes instance.
+ * @see ForestEnemySpawnableGround
+ * @see Weather
+ * @see WeatherControllable
+ */
 public class Bushes<R extends RedWolf> extends ForestEnemySpawnableGround<R> {
 
   /**
-   * Constructor.
+   * Constructor to create a Bushes instance.
+   *
+   * @param redWolfSpawner The spawner for Red Wolves associated with this Bushes instance.
    */
   public Bushes(Spawner<R> redWolfSpawner){
-    super('m', 30,redWolfSpawner);
+    super('m', 30, redWolfSpawner);
   }
 
+  /**
+   * Updates the Red Wolf spawning rate based on the current weather conditions and informs any associated ForestEnemies.
+   *
+   * @param weather The current weather.
+   * @param display The display interface to show weather-related messages.
+   */
   @Override
   public void updateWeatherMode(Weather weather, Display display) {
     if (weather == Weather.SUNNY){
@@ -25,8 +48,8 @@ public class Bushes<R extends RedWolf> extends ForestEnemySpawnableGround<R> {
       super.setSpawnRate(45);
       display.println("The red wolves are becoming more active.");
     }
-    for(WeatherControllable forestEnemy: forestEnemyList){
-      forestEnemy.updateWeatherMode(weather,display);
+    for (WeatherControllable forestEnemy: forestEnemyList){
+      forestEnemy.updateWeatherMode(weather, display);
     }
   }
 }
