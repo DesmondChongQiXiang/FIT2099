92579938a2295a968bac3ef951785c621dde4317
DesmondChongQiXiang <91426701+qixiangggg@users.noreply.github.com>
2023-10-14 18:00:07 +0800

    REQ3 (refactor monologueOptions and allowableActions of Blacksmith and created a monologueOptions class)

--- src/game/actions/ListenMonologueAction.java
+++ src/game/actions/ListenMonologueAction.java
@@ -3,20 +3,21 @@ package game.actions;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.MonologueOptions;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class ListenMonologueAction extends Action {
     private Random rand = new Random();
-    private ArrayList<String> monologueList;
+    private MonologueOptions monologueOptions;
 
-    public ListenMonologueAction(ArrayList<String> monologueList){
-        this.monologueList = monologueList;
+    public ListenMonologueAction(MonologueOptions monologueOptions){
+        this.monologueOptions = monologueOptions;
     }
     @Override
     public String execute(Actor actor, GameMap map) {
-        return monologueList.get(rand.nextInt(monologueList.size()));
+        return monologueOptions.chooseOption();
     }
 
     @Override
--- src/game/actors/Blacksmith.java
+++ src/game/actors/Blacksmith.java
@@ -4,33 +4,25 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.actors.Behaviour;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
-import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
-import game.actions.AttackAction;
 import game.actions.ListenMonologueAction;
-import game.actors.enemies.Enemy;
-import game.behaviours.FollowBehaviour;
-import game.behaviours.WanderBehaviour;
 import game.capabilities.Ability;
 import game.capabilities.Status;
-import game.grounds.Gate;
-import game.weathers.WeatherControllable;
-import game.weathers.WeatherManager;
+
 
 import java.util.ArrayList;
 
 public class Blacksmith extends Actor {
-    private ArrayList<String> monologueOptions;
+    private MonologueOptions monologueOptions;
 
     public Blacksmith() {
         super("Blacksmith", 'B', 0);
         this.addCapability(Ability.UPGRADE_EQUIPMENT);
-        this.monologueOptions = new ArrayList<>();
-        monologueOptions.add("I used to be an adventurer like you, but then …. Nevermind, let’s get back to smithing.");
-        monologueOptions.add("It’s dangerous to go alone. Take my creation with you on your adventure!");
-        monologueOptions.add("Ah, it’s you. Let’s get back to make your weapons stronger.");
+        this.monologueOptions = new MonologueOptions();
+        monologueOptions.addOption("I used to be an adventurer like you, but then …. Nevermind, let’s get back to smithing.");
+        monologueOptions.addOption("It’s dangerous to go alone. Take my creation with you on your adventure!");
+        monologueOptions.addOption("Ah, it’s you. Let’s get back to make your weapons stronger.");
     }
 
     @Override
@@ -42,16 +34,25 @@ public class Blacksmith extends Actor {
     public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
         ActionList actions = new ActionList();
         if (otherActor.hasCapability(Ability.LISTEN_STORY)) {
-            if (otherActor.hasCapability(Ability.USE_GREATKNIFE)) {
-                monologueOptions.add("Hey now, that’s a weapon from a foreign land that I have not seen for so long. I can upgrade it for you if you wish.");
-            }
-            if (otherActor.hasCapability(Status.BOSS_DEFEATED)) {
-                monologueOptions.add("Somebody once told me that a sacred tree rules the land beyond the ancient woods until this day.");
-            } else {
-                monologueOptions.add("Beyond the burial ground, you’ll come across the ancient woods ruled by Abxervyer. Use my creation to slay them and proceed further!");
-            }
+            this.addMonologueOptions(otherActor);
             actions.add(new ListenMonologueAction(monologueOptions));
         }
         return actions;
+    }
+
+    public void addMonologueOptions(Actor listener){
+        this.monologueOptions.clearOption();
+        monologueOptions.addOption("I used to be an adventurer like you, but then …. Nevermind, let’s get back to smithing.");
+        monologueOptions.addOption("It’s dangerous to go alone. Take my creation with you on your adventure!");
+        monologueOptions.addOption("Ah, it’s you. Let’s get back to make your weapons stronger.");
+        if (listener.hasCapability(Ability.USE_GREATKNIFE)) {
+            monologueOptions.addOption("Hey now, that’s a weapon from a foreign land that I have not seen for so long. I can upgrade it for you if you wish.");
+        }
+        if (listener.hasCapability(Status.BOSS_DEFEATED)) {
+            monologueOptions.addOption("Somebody once told me that a sacred tree rules the land beyond the ancient woods until this day.");
+        } else {
+            monologueOptions.addOption("Beyond the burial ground, you’ll come across the ancient woods ruled by Abxervyer. Use my creation to slay them and proceed further!");
+        }
+
     }
 }
