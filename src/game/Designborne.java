8b29156a2e3f2bac07979d436ef6b5f4dd9d8342
Chonghow29 <cong0027@student.monash.edu>
2023-10-15 22:47:08 +0800

    REQ5 (Create list to differentiate different classes to deal with after player dead)

--- src/game/Designborne.java
+++ src/game/Designborne.java
@@ -1,5 +1,8 @@
 package game;
 
+import edu.monash.fit2099.engine.actors.Behaviour;
+import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.FancyGroundFactory;
@@ -21,6 +24,7 @@ import game.actors.enemies.villageenemy.WanderingUndead;
 import game.displays.FancyMessage;
 import game.grounds.*;
 import game.grounds.Void;
+import game.grounds.environments.EnemySpawnableGround;
 import game.grounds.environments.forest.ForestBush;
 import game.grounds.environments.forest.ForestEnemySpawnableGround;
 import game.grounds.environments.forest.ForestHut;
@@ -44,12 +48,20 @@ import java.util.List;
 public class Designborne {
 
     private World world;
-    private HashMap<String,GameMap> gameMapHashMap = new HashMap<>();
+    private HashMap<String,GameMap> gameMapHashMap;
+    private ArrayList<EnemySpawnableGround> enemySpawnableGroundList;
+    private ArrayList<Gate> gateList;
     private ArrayList<WeatherControllable> forestEnemySpawnableGroundList;
+    private Player player;
+    private ForestWatcher forestWatcher;
 
     public Designborne(World world) {
         this.world = world;
+        this.gameMapHashMap = new HashMap<>();
+        this.enemySpawnableGroundList = new ArrayList<>();
         this.forestEnemySpawnableGroundList = new ArrayList<>();
+        this.player = new Player("The Abstracted One", '@', 150, 200);
+        this.gateList = new ArrayList<>();
     }
 
     public void createGameMap(){
@@ -169,56 +181,85 @@ public class Designborne {
     }
 
     public void createGround() {
-        gameMapHashMap.get("The Abandoned Village").at(10,8).setGround(new WanderingUndeadGraveyard<WanderingUndead>(WanderingUndead.SPAWNER));
+        EnemySpawnableGround<WanderingUndead> abandonedVillageGraveyard = new WanderingUndeadGraveyard<>(WanderingUndead.SPAWNER);
+        gameMapHashMap.get("The Abandoned Village").at(10,8).setGround(abandonedVillageGraveyard);
+        enemySpawnableGroundList.add(abandonedVillageGraveyard);
 
-        gameMapHashMap.get("The Burial Ground").at(21,11).setGround(new HollowSoldierGraveyard<HollowSoldier>(HollowSoldier.SPAWNER));
+        EnemySpawnableGround<HollowSoldier> burialGroundGraveyard = new HollowSoldierGraveyard<HollowSoldier>(HollowSoldier.SPAWNER);
+        gameMapHashMap.get("The Burial Ground").at(21,11).setGround(burialGroundGraveyard);
+        enemySpawnableGroundList.add(burialGroundGraveyard);
+
         Gate abandonedVillageGate = new Gate();
         gameMapHashMap.get("The Abandoned Village").at(31, 5).setGround(abandonedVillageGate);
         abandonedVillageGate.addTravelAction(new TravelAction(gameMapHashMap.get("The Burial Ground").at(22, 7),"The Burial Ground"));
+        gateList.add(abandonedVillageGate);
+
         Gate burialGroundGate = new Gate();
         gameMapHashMap.get("The Burial Ground").at(23,7).setGround(burialGroundGate);
         burialGroundGate.addTravelAction(new TravelAction(gameMapHashMap.get("The Abandoned Village").at(31, 5),"The Abandoned Village"));
+        gateList.add(burialGroundGate);
 
-        ForestEnemySpawnableGround<ForestEnemy> ancientWoodHuts = new ForestHut(ForestKeeper.SPAWNER);
-        ForestEnemySpawnableGround<ForestEnemy> ancientWoodBush = new ForestBush(RedWolf.SPAWNER);
-        gameMapHashMap.get("The Ancient Woods").at(29,0).setGround(ancientWoodHuts);
+        ForestEnemySpawnableGround<ForestKeeper> ancientWoodHut = new ForestHut<>(ForestKeeper.SPAWNER);
+        gameMapHashMap.get("The Ancient Woods").at(29,0).setGround(ancientWoodHut);
+        forestEnemySpawnableGroundList.add(ancientWoodHut);
+        enemySpawnableGroundList.add(ancientWoodHut);
+
+        ForestEnemySpawnableGround<RedWolf> ancientWoodBush = new ForestBush<>(RedWolf.SPAWNER);
         gameMapHashMap.get("The Ancient Woods").at(15,11).setGround(ancientWoodBush);
+        forestEnemySpawnableGroundList.add(ancientWoodBush);
+        enemySpawnableGroundList.add(ancientWoodBush);
+
         Gate burialGroundGate2 = new Gate();
         burialGroundGate2.addTravelAction(new TravelAction(gameMapHashMap.get("The Ancient Woods").at(21, 4),"The Ancient Woods"));
         gameMapHashMap.get("The Burial Ground").at(30, 14).setGround(burialGroundGate2);
+        gateList.add(burialGroundGate2);
+
         Gate ancientWoodsGate = new Gate();
         ancientWoodsGate.addTravelAction(new TravelAction(gameMapHashMap.get("The Burial Ground").at(10, 14),"The Burial Ground"));
         gameMapHashMap.get("The Ancient Woods").at(10,11).setGround(ancientWoodsGate);
+        gateList.add(ancientWoodsGate);
 
-        ForestEnemySpawnableGround<ForestEnemy> abxervyerHut1 = new ForestHut(ForestKeeper.SPAWNER);
-        ForestEnemySpawnableGround<ForestEnemy> abxervyerHut2 = new ForestHut(ForestKeeper.SPAWNER);
-        ForestEnemySpawnableGround<ForestEnemy> abxervyerBush1 = new ForestBush(RedWolf.SPAWNER);
-        ForestEnemySpawnableGround<ForestEnemy> abxervyerBush2 = new ForestHut(ForestKeeper.SPAWNER);
+        ForestEnemySpawnableGround<ForestKeeper> abxervyerHut1 = new ForestHut<>(ForestKeeper.SPAWNER);
         gameMapHashMap.get("The Abxervyer").at(10, 19).setGround(abxervyerHut1);
-        gameMapHashMap.get("The Abxervyer").at(32, 0).setGround(abxervyerHut2);
-        gameMapHashMap.get("The Abxervyer").at(11, 13).setGround(abxervyerBush1);
-        gameMapHashMap.get("The Abxervyer").at(36, 18).setGround(abxervyerBush2);
-        forestEnemySpawnableGroundList.add(ancientWoodHuts);
-        forestEnemySpawnableGroundList.add(ancientWoodBush);
         forestEnemySpawnableGroundList.add(abxervyerHut1);
+        enemySpawnableGroundList.add(abxervyerHut1);
+
+        ForestEnemySpawnableGround<ForestKeeper> abxervyerHut2 = new ForestHut<>(ForestKeeper.SPAWNER);
+        gameMapHashMap.get("The Abxervyer").at(32, 0).setGround(abxervyerHut2);
         forestEnemySpawnableGroundList.add(abxervyerHut2);
+        enemySpawnableGroundList.add(abxervyerHut2);
+
+        ForestEnemySpawnableGround<RedWolf> abxervyerBush1 = new ForestBush<>(RedWolf.SPAWNER);
+        gameMapHashMap.get("The Abxervyer").at(11, 13).setGround(abxervyerBush1);
         forestEnemySpawnableGroundList.add(abxervyerBush1);
+        enemySpawnableGroundList.add(abxervyerBush1);
+
+        ForestEnemySpawnableGround<ForestKeeper> abxervyerBush2 = new ForestHut<>(ForestKeeper.SPAWNER);
+        gameMapHashMap.get("The Abxervyer").at(36, 18).setGround(abxervyerBush2);
         forestEnemySpawnableGroundList.add(abxervyerBush2);
+        enemySpawnableGroundList.add(abxervyerBush2);
+
         Gate ancientWoodsGate2 = new Gate();
         ancientWoodsGate2.addTravelAction(new TravelAction(gameMapHashMap.get("The Abxervyer").at(39, 13), "Abxervyer, The Forest Watcher"));
         gameMapHashMap.get("The Ancient Woods").at(0, 6).setGround(ancientWoodsGate2);
-//        Gate abxvyerGate1 = new Gate();
-//        abxvyerGate1.addTravelAction(new TravelAction(gameMapHashMap.get("The Ancient Woods").at(21, 4),"The Ancient Woods"));
-//
-//        abxvyerGate1.addTravelAction(new TravelAction(gameMapHashMap.get("The Overgrown Sanctuary").at(8, 4),"The Overgrown Sanctuary"));
+        gateList.add(ancientWoodsGate2);
+
         Gate overgrownSanctuaryGate1 = new Gate();
         overgrownSanctuaryGate1.addTravelAction(new TravelAction(gameMapHashMap.get("The Abxervyer").at(39, 13), "Abxervyer, The Forest Watcher"));
         gameMapHashMap.get("The Overgrown Sanctuary").at(7, 4).setGround(overgrownSanctuaryGate1);
-        gameMapHashMap.get("The Overgrown Sanctuary").at(5,11).setGround(new HollowSoldierGraveyard<HollowSoldier>(HollowSoldier.SPAWNER));
-        SanctuaryEnemySpawnableGround<SanctuaryEnemy> overgrownSanctuaryHut = new SanctuaryHut(EldentreeGuardian.SPAWNER);
-        SanctuaryEnemySpawnableGround<SanctuaryEnemy> overgrownSanctuaryBush = new SanctuaryBush(LivingBranch.SPAWNER);
+        gateList.add(overgrownSanctuaryGate1);
+
+        EnemySpawnableGround<HollowSoldier> overgrownSanctuaryGraveyard = new HollowSoldierGraveyard<>(HollowSoldier.SPAWNER);
+        gameMapHashMap.get("The Overgrown Sanctuary").at(5,11).setGround(overgrownSanctuaryGraveyard);
+        enemySpawnableGroundList.add(overgrownSanctuaryGraveyard);
+
+        SanctuaryEnemySpawnableGround<EldentreeGuardian> overgrownSanctuaryHut = new SanctuaryHut<>(EldentreeGuardian.SPAWNER);
         gameMapHashMap.get("The Overgrown Sanctuary").at(9, 7).setGround(overgrownSanctuaryHut);
+        enemySpawnableGroundList.add(overgrownSanctuaryHut);
+
+        SanctuaryEnemySpawnableGround<LivingBranch> overgrownSanctuaryBush = new SanctuaryBush<>(LivingBranch.SPAWNER);
         gameMapHashMap.get("The Overgrown Sanctuary").at(22, 7).setGround(overgrownSanctuaryBush);
+        enemySpawnableGroundList.add(overgrownSanctuaryBush);
     }
 
     public void createItem() {
@@ -235,7 +276,6 @@ public class Designborne {
     }
 
     public void createActor() {
-        Player player = new Player("The Abstracted One", '@', 150, 200);
         world.addPlayer(player,gameMapHashMap.get("The Abandoned Village").at(29, 5));
 
         Blacksmith blacksmith = new Blacksmith();
@@ -244,12 +284,16 @@ public class Designborne {
         Traveller traveller = new Traveller();
         gameMapHashMap.get("The Ancient Woods").at(20,3).addActor(traveller);
 
-        Gate abxvyerGate1 = new Gate();
-        abxvyerGate1.addTravelAction(new TravelAction(gameMapHashMap.get("The Ancient Woods").at(21, 4),"The Ancient Woods"));
-        abxvyerGate1.addTravelAction(new TravelAction(gameMapHashMap.get("The Overgrown Sanctuary").at(8, 4),"The Overgrown Sanctuary"));
         WeatherManager weatherManager = new WeatherManager(Weather.SUNNY,forestEnemySpawnableGroundList);
-        ForestWatcher forestWatcher = new ForestWatcher(weatherManager, abxvyerGate1);
+        this.forestWatcher = new ForestWatcher(weatherManager, createForestWatcherGate());
         gameMapHashMap.get("The Abxervyer").at((gameMapHashMap.get("The Abxervyer").getXRange().max())/2,(gameMapHashMap.get("The Abxervyer").getYRange().max())/2).addActor(forestWatcher);
+    }
+
+    public Gate createForestWatcherGate() {
+        Gate abxvyerGate = new Gate();
+        abxvyerGate.addTravelAction(new TravelAction(gameMapHashMap.get("The Ancient Woods").at(21, 4),"The Ancient Woods"));
+        abxvyerGate.addTravelAction(new TravelAction(gameMapHashMap.get("The Overgrown Sanctuary").at(8, 4),"The Overgrown Sanctuary"));
+        return abxvyerGate;
     }
 
     public void showMessage(){
@@ -263,7 +307,26 @@ public class Designborne {
         }
     }
 
+    public void respawnPlayer(){
+        player.heal(player.getAttributeMaximum(BaseActorAttributes.HEALTH));
+        player.modifyAttribute(BaseActorAttributes.STAMINA, ActorAttributeOperations.UPDATE, player.getAttributeMaximum(BaseActorAttributes.STAMINA));
+        gameMapHashMap.get("The Abandoned Village").at(29,5).addActor(player);
+    }
+
     public World getWorld() {
         return world;
+    }
+
+    public void playerDead(){
+        for(EnemySpawnableGround enemySpawnableGround:enemySpawnableGroundList){
+            enemySpawnableGround.setPlayerDead();
+        }
+        for (GameMap map : gameMapHashMap.values()){
+            map.tick();
+        }
+        for(Gate gate:gateList){
+            gate.lockGate();
+        }
+        forestWatcher.heal(forestWatcher.getAttributeMaximum(BaseActorAttributes.HEALTH));
     }
 }
